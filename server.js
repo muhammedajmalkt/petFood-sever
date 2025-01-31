3f6bea1691786c4323cb34f6d68a8f8283e90bf5
muhammedajmalkt <muhammedajmalkt@gmail.com>
2025-01-31 10:27:45 +0530

    upadated

--- server.js
+++ server.js
@@ -10,10 +10,16 @@ const wishlistRouter=require("./routers/wishlistRouter")
 const adminRouter=require("./routers/adminRouter")
 const errorHandler = require("./middlewares/errorHandler")
 const cookieParser = require("cookie-parser")
+const path = require("path");
 
 const app=express()
 app.use(express.json())
 // app.use(express.urlencoded({extended:true}))
+
+app.use(express.static(path.join(__dirname, "build")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
 
 const option={
     origin: "https://petfood-jzut.onrender.com",
