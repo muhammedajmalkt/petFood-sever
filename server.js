74adf14f8c8535d06756dbc9249d788d9ac1063a
muhammedajmalkt <muhammedajmalkt@gmail.com>
2025-01-31 10:38:21 +0530

    upadated

--- server.js
+++ server.js
@@ -16,10 +16,10 @@ const app=express()
 app.use(express.json())
 // app.use(express.urlencoded({extended:true}))
 
-app.use(express.static(path.join(__dirname, "build")));
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
-});
+// app.use(express.static(path.join(__dirname, "build")));
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "build", "index.html"));
+// });
 
 const option={
     origin: "https://petfood-jzut.onrender.com",
